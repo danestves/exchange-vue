ca0f22fefced7b451a1b3693372b96a88f510b47
Daniel Esteves <estevesd8@gmail.com>
2020-03-28 18:27:51 -0400

    Base styles for app

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <template>
-  <main></main>
+  <main>
+    <x-header />
+    <x-assets-table-vue />
+  </main>
 </template>
 
 <script>
+import xHeader from "@/components/xHeader";
+import xAssetsTableVue from "./components/xAssetsTable.vue";
+
 export default {
-  name: "App"
+  name: "App",
+  components: {
+    xHeader,
+    xAssetsTableVue
+  }
 };
 </script>
---
+++ src/components/xHeader.vue
@@ -0,0 +1,29 @@
+<template>
+  <header class="w-screen shadow">
+    <nav>
+      <nav class="flex flex-wrap items-center justify-between p-6 bg-green-400">
+        <div class="flex items-center flex-shrink-0 mr-6 text-white">
+          <x-icon class="mr-2" />
+          <span class="text-xl font-semibold tracking-tight">Exchange</span>
+        </div>
+
+        <div
+          class="flex-grow hidden block w-full sm:block lg:flex lg:items-center lg:w-auto"
+        >
+          <div class="text-sm lg:flex-grow"></div>
+        </div>
+      </nav>
+    </nav>
+  </header>
+</template>
+
+<script>
+import xIcon from "@/components/xIcon";
+
+export default {
+  name: "xHeader",
+  components: {
+    xIcon
+  }
+};
+</script>
