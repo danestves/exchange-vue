764c8c96437c8fe087e24a62e24c2605d78fd158
Daniel Esteves <estevesd8@gmail.com>
2020-03-29 00:34:06 -0400

    Deleting console statements

--- src/utils/api.js
+++ src/utils/api.js
@@ -7,14 +7,18 @@ async function getAssets() {
   return await axios
     .get(`${url}/assets?limit=20`)
     .then(res => res.data.data)
-    .catch(err => console.error(err))
+    .catch(err => {
+      throw new Error(err)
+    })
 }
 
 async function getAsset(coin) {
   return await axios
     .get(`${url}/assets/${coin}`)
     .then(res => res.data.data)
-    .catch(err => console.error(err))
+    .catch(err => {
+      throw new Error(err)
+    })
 }
 
 async function getAssetHistory(coin) {
@@ -26,21 +30,27 @@ async function getAssetHistory(coin) {
   return await axios
     .get(`${url}/assets/${coin}/history?interval=h1&start=${start}&end=${end}`)
     .then(res => res.data.data)
-    .catch(err => console.error(err))
+    .catch(err => {
+      throw new Error(err)
+    })
 }
 
 async function getMarkets(coin) {
   return await axios
     .get(`${url}/assets/${coin}/markets?limit=5`)
     .then(res => res.data.data)
-    .catch(err => console.error(err))
+    .catch(err => {
+      throw new Error(err)
+    })
 }
 
 async function getExchange(id) {
   return await axios
     .get(`${url}/exchanges/${id}`)
     .then(res => res.data.data)
-    .catch(err => console.error(err))
+    .catch(err => {
+      throw new Error(err)
+    })
 }
 
 export { getAssets, getAsset, getAssetHistory, getMarkets, getExchange }
